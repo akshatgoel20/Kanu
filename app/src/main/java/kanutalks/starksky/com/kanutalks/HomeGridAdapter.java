a2bc9ef11820a494e69d216cc62e846d96020351
Akshat Goel <akshatishu@gmail.com>
2015-06-16 21:24:13 +0530

    Added image with text .Grid

--- app/src/main/java/kanutalks/starksky/com/kanutalks/HomeGridAdapter.java
+++ app/src/main/java/kanutalks/starksky/com/kanutalks/HomeGridAdapter.java
@@ -1,23 +1,28 @@
 package kanutalks.starksky.com.kanutalks;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by akshat on 15/06/15.
  */
 public class HomeGridAdapter extends BaseAdapter {
 
+    private static LayoutInflater inflater = null;
 
     private Context mContext;
+
     // Constructor
     public HomeGridAdapter(Context c) {
         mContext = c;
     }
+
     @Override
     public int getCount() {
         return mThumbIds.length;
@@ -33,22 +38,34 @@ public class HomeGridAdapter extends BaseAdapter {
         return 0;
     }
 
+    public class Holder {
+        TextView tv;
+        ImageView imageView;
+    }
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
 
-        if (convertView == null) {
-            imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(150, 150));
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setPadding(8, 8, 8, 8);
-        }
-        else
-        {
-            imageView = (ImageView) convertView;
-        }
-        imageView.setImageResource(mThumbIds[position]);
-        return imageView;
+        Holder holder = new Holder();
+
+        inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View cellView;
+
+        cellView = inflater.inflate(R.layout.layout_home_cell, null);
+
+        holder.imageView = (ImageView) cellView.findViewById(R.id.imageView1);
+        holder.tv = (TextView) cellView.findViewById(R.id.textView1);
+
+
+      /*  holder.imageView.setLayoutParams(new GridView.LayoutParams(150, 150));
+        holder.imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        holder.imageView.setPadding(8, 8, 8, 8);*/
+
+
+        holder.imageView.setImageResource(mThumbIds[position]);
+        holder.tv.setText(imgtexts[position]);
+        return cellView;
     }
 
 
@@ -59,6 +76,12 @@ public class HomeGridAdapter extends BaseAdapter {
             R.drawable.aeroplane, R.drawable.carrom,
             R.drawable.happy, R.drawable.toilet,
             R.drawable.myroom
+
+    };
+
+    public String[] imgtexts = {"Food", "Drinks", "Fun"
+            , "Medical", "Travel", "Games",
+            "Emoticons", "Daily Needs", "My Room"
 
     };
 }
