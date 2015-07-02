51819f9a6dc1669a93a7e9e2ed059cf077d9fb0f
akshat <akshatishu@gmail.com>
2015-07-02 10:01:16 +0530

    Minor Fix

--- app/src/main/java/kanutalks/starksky/com/kanutalks/Category.java
+++ app/src/main/java/kanutalks/starksky/com/kanutalks/Category.java
@@ -158,6 +158,7 @@ public class Category extends ActionBarActivity {
                 }
                 break;
         }
+        //
         new CategoryGridAdapter(mThumbids,imgtexts);
         t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
